4e87d5a458921ccec5cbef97d9b1e18e1c31a985
Parth Divekar <parthdivekar987@gmail.com>
2026-05-19 10:55:50 +0530

    modified testlistener

--- src/test/java/com/onboarding/automation/listeners/TestListener.java
+++ src/test/java/com/onboarding/automation/listeners/TestListener.java
@@ -1,3 +1,29 @@
+//package com.onboarding.automation.listeners;
+//
+//import com.onboarding.automation.base.BaseTest;
+//import com.onboarding.automation.utils.ScreenshotUtil;
+//
+//import org.testng.ITestListener;
+//import org.testng.ITestResult;
+//
+//import org.openqa.selenium.WebDriver;
+//
+//public class TestListener implements ITestListener {
+//
+//    @Override
+//    public void onTestFailure(ITestResult result) {
+//
+//        // Get test class object
+//        Object testClass = result.getInstance();
+//
+//        // Get driver from BaseTest
+//        WebDriver driver = ((BaseTest) testClass).driver;
+//
+//        // Capture screenshot
+//        ScreenshotUtil.captureScreenshot(driver, result.getName());
+//    }
+//}
+
 package com.onboarding.automation.listeners;
 
 import com.onboarding.automation.base.BaseTest;
@@ -5,21 +31,14 @@ import com.onboarding.automation.utils.ScreenshotUtil;
 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import org.openqa.selenium.WebDriver;
 
 public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-
-        // Get test class object
         Object testClass = result.getInstance();
-
-        // Get driver from BaseTest
         WebDriver driver = ((BaseTest) testClass).driver;
-
-        // Capture screenshot
         ScreenshotUtil.captureScreenshot(driver, result.getName());
     }
 }
